0953b83a212f4f4e81f6848cb33da29cd8f93a50
Sasikiran TT <sasikiran.tt3011@gmail.com>
2026-05-25 14:05:50 +0530

    Port chnage

--- ai-engine/app/main.py
+++ ai-engine/app/main.py
@@ -35,4 +35,6 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
