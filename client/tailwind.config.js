eaba97199342ef0d7379590fd997a482d4af0215
Sasikiran TT <sasikiran.tt3011@gmail.com>
2026-06-03 01:30:10 +0530

    chore(client): update dependencies, tailwind config, and next configs

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#FF4F21",
+      },
+      fontFamily: {
+        sans: ["var(--font-poppins)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
+    },
+  },
+  plugins: [],
+};
