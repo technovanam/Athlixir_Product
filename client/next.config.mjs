04d0687e8b29b2ba4d7ba1357c7f11878c96bd77
Sasikiran TT <sasikiran.tt3011@gmail.com>
2026-06-13 14:10:31 +0530

    config: add eslint dirs config to next.config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    dirs: ['app'],
   },
 };
 
